5deb717b6f8b30831d20f580a6b8dd4514c6274d
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-03-14 16:43:09 +0100

    Add: palindroma word - finish project

--- palindroma/script.js
+++ palindroma/script.js
@@ -3,16 +3,24 @@
 //Creare una funzione per capire se la parola inserita è palindroma
 //Su internet ci sono soluzioni con join e reverse, non potete usarle!
 
-
-
 //Chiedere all’utente di inserire una parola
 const userWord = prompt("Inserisci una parola");
-
-
 
 //Creo costante 'risultato' in cui evoco funzione con parametro il valore
 //inserito dall'utente
 const result = isPalindrome(userWord)
+
+//Infine creo alert, se il risultato della funzione per la parola palindroma 
+//applicata a userWord restituisce 'true', alert dirà "La parola è palindroma"
+//altrimenti la parola non è palindroma.
+if (result === true){
+    alert("La parola è palindroma")
+}
+else {
+    alert("la parola non è palindroma")
+}
+
+//FUNCTIONS:
 
 // Funzione per capire se la parola inserita è palindroma:
 function isPalindrome(word) {
@@ -20,21 +28,14 @@ function isPalindrome(word) {
         
         // Creo condizione in cui se i caratteri non corrispondono, 
         // il ritorno restituisce 'false' altrimenti se è palindroma 'true'
-        if (word[i] !== word[word.length - 1 - i]) {
-            
+        if (word[i] !== word[word.length - 1 - i]) {  
             return false;
-
-
         }
         else {
             return true;
-
         }
-    }
-    
-    
+    }  
 }
 
-
 //verifica:
-console.log(result);
+console.log(userWord, result);
