fa8685db110efbe50359e52f42292abd7ac3ea5e
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-03-14 15:16:28 +0100

    Add: first part 'pari e dispari'

--- pari&dispari/script.js
+++ pari&dispari/script.js
@@ -4,3 +4,26 @@
 //Sommiamo i due numeri
 //Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 //Dichiariamo chi ha vinto.
+
+
+//Chiediamo all'utente se sceglie pari o dispari
+const playerOddOrEven = prompt("Scegli: dispari o pari");
+
+//Chiediamo all'utente di inserire un numero da 1 a 5.
+const playerNumber = parseInt(prompt("Scegli un numero da 1 a 5"));
+
+//assegno a cpu la funzione creta per generare numeri random.
+let cpuChoice = randomNumber();
+
+//Genera un numero casuale da 1 a 5 per la CPU
+function randomNumber(min, max) { 
+    return Math.floor(Math.random() * 5) + 1; 
+}
+
+
+
+console.log(cpuChoice);
+
+    
+
+
